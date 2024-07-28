fc034ba402b33d40295c2829b84b2c69381b3e6b
Dosymzhan <do5ymzh4n@gmail.com>
2024-07-28 16:37:08 +0500

    done start and shutdown server

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"syscall"
+	"time"
+
 	"github.com/julienschmidt/httprouter"
 	"github.com/senizdegen/sdu-housing/api-gateway/internal/config"
 	"github.com/senizdegen/sdu-housing/api-gateway/pkg/cache/freecache"
+	"github.com/senizdegen/sdu-housing/api-gateway/pkg/handlers/metric"
 	"github.com/senizdegen/sdu-housing/api-gateway/pkg/jwt"
 	"github.com/senizdegen/sdu-housing/api-gateway/pkg/logging"
+	"github.com/senizdegen/sdu-housing/api-gateway/pkg/shutdown"
 )
 
 func main() {
@@ -27,6 +39,62 @@ func main() {
 	logger.Println("helpers initializing")
 	jwtHelper := jwt.NewHelper(refreshTokenCache, logger)
 
+	logger.Println("create and register handlers")
+
+	metricHandler := metric.Handler{Logger: logger}
+	metricHandler.Register(router)
+
 	_ = router
 	_ = jwtHelper
+
+	start(router, logger, cfg)
+}
+
+func start(router *httprouter.Router, logger logging.Logger, cfg *config.Config) {
+	var server *http.Server
+	var listener net.Listener
+
+	if cfg.Listen.Type == "sock" {
+		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logger.Fatal(err)
+		}
+		socketPath := path.Join(appDir, "app.sock")
+		logger.Infof("socket path: %s", socketPath)
+
+		logger.Info("create and listen unix socket")
+		listener, err = net.Listen("unix", socketPath)
+		if err != nil {
+			logger.Fatal(err)
+		}
+	} else {
+		logger.Infof("bind application to host: %s and port: %s", cfg.Listen.BindIP, cfg.Listen.Port)
+
+		var err error
+
+		listener, err = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIP, cfg.Listen.Port))
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}
+
+	server = &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	go shutdown.Graceful([]os.Signal{syscall.SIGABRT, syscall.SIGQUIT, syscall.SIGHUP, os.Interrupt, syscall.SIGTERM},
+		server)
+
+	logger.Println("application initialized and started")
+
+	if err := server.Serve(listener); err != nil {
+		switch {
+		case errors.Is(err, http.ErrServerClosed):
+			logger.Warn("server shutdown")
+		default:
+			logger.Fatal(err)
+		}
+	}
 }
